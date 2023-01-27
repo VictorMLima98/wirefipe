1999fc687351e3ffaff2f55cf63e3aee8728d7a8
Victor Lima <victor.lima@devsquad.com>
2023-01-27 18:02:51 -0300

    Install TALL stack

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name') }}</title>
+
+        @wireUiScripts
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+        @livewireStyles
+    </head>
+    <body class="antialiased">
+
+        @livewireScripts
+    </body>
+</html>
