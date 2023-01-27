efbefd34de5c1af1267677769434f2e9a9448b8b
Victor Lima <victor.lima@devsquad.com>
2023-01-27 19:39:31 -0300

    Create dark mode and hero

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,11 +1,11 @@
-<nav class="bg-secondary-50 shadow" x-data="{
+<nav class="bg-secondary-900 shadow" x-data="{
     mobileMenu: false
 }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 justify-between">
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <button type="button"
-                    class="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-500"
+                    class="inline-flex items-center justify-center rounded-md p-2 text-secondary-400 hover:text-secondary-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-secondary-500"
                     aria-controls="mobile-menu" aria-expanded="false" x-on:click="mobileMenu = !mobileMenu">
                     <span class="sr-only">Open main menu</span>
                     <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
@@ -19,9 +19,9 @@
                 <div class="hidden sm:ml-6 sm:flex sm:space-x-8 text-lg">
                     @foreach ($links as $link)
                         <a href="{{ $link['href'] }}" @class([
-                            'inline-flex items-center border-b-2 border-primary-500 text-secondary-900' => $link['active'],
-                            'border-transparent text-secondary-500 hover:border-secondary-300 hover:text-secondary-700' => !$link['active'],
-                            'inline-flex items-center border-b-2 px-1 pt-1 font-medium'])>
+                            'inline-flex items-center border-b-2 border-primary-500 text-secondary-50' => $link['active'],
+                            'border-transparent text-secondary-100 hover:border-secondary-200 hover:text-secondary-100' => !$link['active'],
+                            'inline-flex items-center border-b-2 px-1 pt-1 font-medium transition-all'])>
                             {{ $link['label'] }}
                         </a>
                     @endforeach
@@ -33,10 +33,10 @@
     <div class="sm:hidden" id="mobile-menu" x-show="mobileMenu" x-cloak x-transition>
         <div class="space-y-1 pt-2 pb-4">
             @foreach ($links as $link)
-                <a href="{{ $link['active'] }}"
+                <a href="{{ $link['href'] }}"
                     @class([
-                        'border-primary-500 bg-primary-50 text-primary-700' => $link['active'],
-                        'border-transparent text-secondary-500 hover:border-secondary-300 hover:bg-secondary-50 hover:text-secondary-700' => !$link['active'],
+                        'border-primary-500 text-primary-700' => $link['active'],
+                        'border-transparent text-secondary-100 hover:border-secondary-300 hover:bg-secondary-50 hover:text-secondary-700' => !$link['active'],
                         'block border-l-4 py-2 pl-3 pr-4 text-base font-medium'])>
                     {{ $link['label'] }}
                 </a>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,6 +14,16 @@
 
 <body class="antialiased">
     <x-header />
+    <div class="relative bg-primary-900">
+        <div class="absolute inset-0">
+            <img class="h-full w-full object-cover" src="{{ asset('assets/static/traffic.jpg') }}" alt="">
+            <div class="absolute inset-0 bg-secondary-900 mix-blend-multiply" aria-hidden="true"></div>
+        </div>
+        <div class="relative mx-auto max-w-7xl py-24 px-6 sm:py-32 lg:px-8">
+            <h1 class="text-4xl font-bold tracking-tight text-primary-300 sm:text-5xl lg:text-6xl">Tabela FIPE</h1>
+            <p class="mt-6 max-w-3xl text-2xl text-primary-50 shadow-lg">Descubra o valor de veículos novos, seminovos e usados na tabela FIPE</p>
+        </div>
+      </div>
     {{ $slot }}
     @livewireScripts
 </body>
